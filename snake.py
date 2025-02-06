de5ba016adc1322ae2dbd60d32b9b0262d602d7e
Leandro Benitez <benitez.bpm@gmail.com>
2025-02-06 17:21:25 -0300

    Código otimizado com adição de funções

--- snake.py
+++ snake.py
@@ -1,34 +1,52 @@
 import curses
 
 def game_loop(window):
+    # Setup Inicial
     curses.curs_set(0)
-    window.border(0)
-    height, width = window.getmaxyx()
-
     personagem = [10,15]
-    window.addch(personagem[0], personagem[1], curses.ACS_DIAMOND)
-    while True:
-        window.timeout(1000)
-        char = window.getch()
-        window.clear()
-        window.border(0)
-        match char:
-            case curses.KEY_UP:
-                personagem[0] -= 1
-            case curses.KEY_RIGHT:
-                personagem[1] += 1
-            case curses.KEY_DOWN:
-                personagem[0] += 1
-            case curses.KEY_LEFT:
-                personagem[1] -= 1
-            case _: # A pessoa não apertou a tecla ou apertou outra tecla
-                pass
-        if (personagem[0] <= 0) or (personagem[0] >= height-1):
-            return
-        if (personagem[1] <= 0) or (personagem[1] >= width-1):
-            return
-        window.addch(personagem[0], personagem[1], curses.ACS_DIAMOND)
 
+    while True:
+        draw_screen(window=window)
+        draw_actor(actor=personagem, window=window)
+        direction = get_new_direction(window=window, timeout=1000)
+        if direction is not None:
+            move_actor(actor=personagem, direction=direction)
+        if actor_hit_border(actor=personagem, window=window):
+            return
+    
+def draw_screen(window):
+    window.clear()
+    window.border(0)
+
+def draw_actor(actor, window):
+    window.addch(actor[0], actor[1], curses.ACS_DIAMOND)
+
+def get_new_direction(window, timeout):
+    window.timeout(timeout)
+    direction = window.getch()
+    if direction in [curses.KEY_UP, curses.KEY_RIGHT, curses.KEY_DOWN, curses.KEY_LEFT]:
+        return direction
+    return None
+
+def move_actor(actor, direction):
+        match direction:
+            case curses.KEY_UP:
+                actor[0] -= 1
+            case curses.KEY_RIGHT:
+                actor[1] += 1
+            case curses.KEY_DOWN:
+                actor[0] += 1
+            case curses.KEY_LEFT:
+                actor[1] -= 1
+
+def actor_hit_border(actor, window):
+    height, width = window.getmaxyx()
+    if (actor[0] <= 0) or (actor[0] >= height-1):
+        return True
+    if (actor[1] <= 0) or (actor[1] >= width-1):
+        return True
+    return False
+    
 if __name__ == '__main__':
     curses.wrapper(game_loop)
     print('Perdeu!')
